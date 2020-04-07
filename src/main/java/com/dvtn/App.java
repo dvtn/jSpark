58241d55ec77d6f68547d06ec3ed02195e2d5604
david.tian <david.tian@live.com>
2020-04-07 12:01:15 +0800

    create a new project named jSPark to experience the spark platform based on Java 8

---
+++ src/main/java/com/dvtn/App.java
@@ -0,0 +1,13 @@
+package com.dvtn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
